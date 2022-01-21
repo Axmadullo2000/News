d51a5697c5b7ef9b81eb1786dfcc7a1de107062a
Axmadullo2000 <axmadullo2000@gmail.com>
2022-01-21 16:04:43 +0500

    News Project

--- js/script.js
+++ js/script.js
@@ -24,3 +24,39 @@ const news = [
   "BOX",
   "AMERICAN FOOTBAL IN EU...",
 ];
+/** 1-task */
+let newsButton = document.querySelector(".promo__btn button");
+
+newsButton.remove();
+/** 2-task */
+let promoGenre = document.querySelector(".promo__genre");
+
+promoGenre.textContent = "UzNews";
+
+promoGenre.style.color = "red";
+
+console.log(promoGenre);
+/** 3-task */
+let promoBackground = document.querySelector(".promo__bg");
+
+promoBackground.classList.add("promoBackground");
+
+
+/** 4-task and 5-task */
+let promoInteractive = document.querySelector("ul.promo__interactive-list");
+news.forEach((item, index, arr) => {
+  let list = document.createElement("li");
+  let deleteElement = document.createElement("div");
+  promoInteractive.append(list);
+  list.textContent = `${index + 1}) ${item}`;
+  list.classList.add("promo__interactive-item");
+  list.append(deleteElement);
+  deleteElement.classList.add("delete");
+  console.log(item);
+});
+console.log(promoInteractive);
+
+
+/** 6-task */
+let readMore = document.querySelector(".promo__btn button");
+readMore.style.borderRadius = "20px";
